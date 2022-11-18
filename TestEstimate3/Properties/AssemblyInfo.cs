dd2678dbf7d6744f9a4115123d43cbcd3a844ed1
bhd71 <m.bronytskyy@gmail.com>
2022-11-19 00:10:58 +0200

    Initial commit

---
+++ TestEstimate3/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestEstimate3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestEstimate3")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("22c1936f-7d7d-4fda-bdd3-d7cca524b7c1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestEstimate3/UnitTest1.cs
@@ -0,0 +1,199 @@
+﻿using AnalaizerClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data;
+
+namespace TestEstimate3
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+
+        string Expresion, ExpectedResult;
+        String actual;
+
+        public TestContext TestContext { get; set; }
+        [DataSource(
+            "System.Data.SqlClient",
+            @"Data Source=GWTN141-10;Initial Catalog=lab1;Trusted_Connection=True",
+            "TestingValues",
+            DataAccessMethod.Sequential
+            )]
+        [TestMethod]
+        public void sum()
+        {
+            Expresion = TestContext.DataRow.Table.Rows[0][1].ToString();
+            ExpectedResult = TestContext.DataRow.Table.Rows[0][2].ToString(); 
+
+
+            AnalaizerClass.expression = Expresion;
+            actual = AnalaizerClass.Estimate();
+
+            try
+            {
+                Assert.AreEqual(ExpectedResult, actual);
+            }
+            catch
+            {
+                System.Console.WriteLine("sum is considered bad");
+            }
+        }
+
+
+        [DataSource(
+            "System.Data.SqlClient",
+            @"Data Source=GWTN141-10;Initial Catalog=lab1;Trusted_Connection=True",
+            "TestingValues",
+            DataAccessMethod.Sequential
+            )]
+        [TestMethod]
+        public void min()
+        {
+            Expresion = TestContext.DataRow.Table.Rows[1][1].ToString();
+            ExpectedResult = TestContext.DataRow.Table.Rows[1][2].ToString();
+
+            AnalaizerClass.expression = Expresion;
+            actual = AnalaizerClass.Estimate();
+
+            try
+            {
+                Assert.AreEqual(ExpectedResult, actual);
+            }
+            catch
+            {
+                System.Console.WriteLine("min is considered bad");
+            }
+
+
+        }
+
+        [DataSource(
+           "System.Data.SqlClient",
+           @"Data Source=GWTN141-10;Initial Catalog=lab1;Trusted_Connection=True",
+           "TestingValues",
+           DataAccessMethod.Sequential
+           )]
+        [TestMethod]
+        public void mult()
+        {
+            Expresion = TestContext.DataRow.Table.Rows[2][1].ToString();
+            ExpectedResult = TestContext.DataRow.Table.Rows[2][2].ToString();
+
+            AnalaizerClass.expression = Expresion;
+            actual = AnalaizerClass.Estimate();
+
+            try
+            {
+                Assert.AreEqual(ExpectedResult, actual);
+            }
+            catch
+            {
+                System.Console.WriteLine("mult is considered bad");
+            }
+
+        } 
+        
+        
+        [DataSource(
+           "System.Data.SqlClient",
+           @"Data Source=GWTN141-10;Initial Catalog=lab1;Trusted_Connection=True",
+           "TestingValues",
+           DataAccessMethod.Sequential
+           )]
+        [TestMethod]
+        public void dev()
+        {
+            Expresion = TestContext.DataRow.Table.Rows[3][1].ToString();
+            ExpectedResult = TestContext.DataRow.Table.Rows[3][2].ToString();
+
+            AnalaizerClass.expression = Expresion;
+            actual = AnalaizerClass.Estimate();
+
+            try
+            {
+                Assert.AreEqual(ExpectedResult, actual);
+            }
+            catch
+            {
+                System.Console.WriteLine("dev is considered bad");
+            }
+
+        }
+        [DataSource(
+           "System.Data.SqlClient",
+           @"Data Source=GWTN141-10;Initial Catalog=lab1;Trusted_Connection=True",
+           "TestingValues",
+           DataAccessMethod.Sequential
+           )]
+        [TestMethod]
+        public void devByNull()
+        {
+            Expresion = TestContext.DataRow.Table.Rows[4][1].ToString();
+            ExpectedResult = TestContext.DataRow.Table.Rows[4][2].ToString();
+
+            AnalaizerClass.expression = Expresion;
+            actual = AnalaizerClass.Estimate();
+
+            try
+            {
+                Assert.AreEqual(ExpectedResult, actual);
+            }
+            catch
+            {
+                System.Console.WriteLine("devByNull is considered bad");
+            }
+
+        }
+        [DataSource(
+           "System.Data.SqlClient",
+           @"Data Source=GWTN141-10;Initial Catalog=lab1;Trusted_Connection=True",
+           "TestingValues",
+           DataAccessMethod.Sequential
+           )]
+        [TestMethod]
+        public void mod()
+        {
+            Expresion = TestContext.DataRow.Table.Rows[5][1].ToString();
+            ExpectedResult = TestContext.DataRow.Table.Rows[5][2].ToString();
+
+            AnalaizerClass.expression = Expresion;
+            actual = AnalaizerClass.Estimate();
+
+            try
+            {
+                Assert.AreEqual(ExpectedResult, actual);
+            }
+            catch
+            {
+                System.Console.WriteLine("mod is considered bad");
+            }
+
+        }
+        [DataSource(
+           "System.Data.SqlClient",
+           @"Data Source=GWTN141-10;Initial Catalog=lab1;Trusted_Connection=True",
+           "TestingValues",
+           DataAccessMethod.Sequential
+           )]
+        [TestMethod]
+        public void modWithRemainder()
+        {
+            Expresion = TestContext.DataRow.Table.Rows[6][1].ToString();
+            ExpectedResult = TestContext.DataRow.Table.Rows[6][2].ToString();
+
+            AnalaizerClass.expression = Expresion;
+            actual = AnalaizerClass.Estimate();
+
+            try
+            {
+                Assert.AreEqual(ExpectedResult, actual);
+            }
+            catch
+            {
+                System.Console.WriteLine("mod with remainder is considered bad");
+            }
+
+        }
+    }
+}
